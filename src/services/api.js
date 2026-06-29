d1967cd23dd9c6846a98435321e5bc2f0d01c0af
Priya050201 <156509998+Priya050201@users.noreply.github.com>
2026-06-29 19:43:16 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
 
-    baseURL: "https://temple-backend-production-07ab.up.railway.app"
+    baseURL: "https://temple-backend-production-07ab.up.railway.app/api/home"
 });
 
 api.interceptors.request.use((config) => {
